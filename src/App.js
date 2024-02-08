bfa6839de65b587c2549f18b0e7b7f2fe2e3ed60
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-02-08 01:25:27 -0600

    Redux toolkit for User Auth + Changes implmnt in Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 // import "./App.css"
 import ProtectedRoutes from "./routes/ProtectedRoutes"
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./app/store"
+import { Provider } from "react-redux"
 import { ThemeProvider } from "@mui/material/styles"
 import { theme } from "./theme/GlobalTheme"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <ProtectedRoutes/>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ProtectedRoutes />
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import * as React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { logout } from "../../features/auth/authSlice"
+
 import { styled } from "@mui/material/styles"
-import { Container } from "@mui/material"
+import { Container, Typography, Menu, MenuItem, IconButton } from "@mui/material"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Button from "@mui/material/Button"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import logo from "../../assets/logo.png"
 
 const StyledToolbar = styled(Toolbar)({
@@ -24,21 +28,40 @@ const Logo = styled("img")({
 })
 
 const StyledButton = styled(Button)({
-  color: "black", // Default color
-  textTransform: "capitalize", // Only capitalize the first letter
+  color: "black",
+  textTransform: "capitalize",
   "&:hover": {
-    color: "grey" // Color on hover
+    color: "grey"
   }
 })
 
 const StyledAppBar = styled(AppBar)({
-  backgroundColor: "white", // Change the color of the navbar to white
-  color: "black", // Set the default color of the text to black
+  backgroundColor: "white",
+  color: "black",
   maxWidth: "100%"
 })
 
 export default function Navbar() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { userEmail, isLoggedIn } = useSelector(state => state.auth)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleLogout = () => {
+    handleClose()
+    dispatch(logout())
+    sessionStorage.clear()
+    navigate("/login")
+  }
 
   return (
     <Container>
@@ -49,10 +72,43 @@ export default function Navbar() {
             <StyledButton onClick={() => navigate("/rent")}>Rent</StyledButton>
             <StyledButton onClick={() => navigate("/sell")}>Sell</StyledButton>
           </NavSection>
-          <Logo src={logo} alt="Logo" onClick={() => navigate("/")} sx={{ cursor: "pointer" }} />
+          <Logo src={logo} alt="Logo" onClick={() => navigate("/")} style={{ cursor: "pointer" }} />
           <NavSection>
-            <StyledButton onClick={() => navigate("/login")}>Log In</StyledButton>
-            <StyledButton onClick={() => navigate("/register")}>Register</StyledButton>
+            {isLoggedIn ? (
+              <>
+                <Typography variant="subtitle1" component="div" sx={{ paddingLeft: 1, cursor: "pointer" }} onClick={handleMenu}>
+                  {userEmail}
+                </Typography>
+                <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleMenu} color="inherit">
+                  <ExpandMoreIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right"
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem>View Favorite Properties</MenuItem>
+                  <MenuItem>View Offer List</MenuItem>
+                  <MenuItem>Edit Profile</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <>
+                <StyledButton onClick={() => navigate("/login")}>Log In</StyledButton>
+                <StyledButton onClick={() => navigate("/register")}>Register</StyledButton>
+              </>
+            )}
           </NavSection>
         </StyledToolbar>
       </StyledAppBar>
