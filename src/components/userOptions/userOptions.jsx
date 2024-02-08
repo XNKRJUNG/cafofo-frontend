ae9877e81b4c429e41ea9e4564bf1ecf26755e6e
Raul Souto <raul.soutos96@gmail.com>
2024-02-08 14:02:43 -0600

    Raul - adding deactivation feature to admin dashboard

--- src/components/userOptions/userOptions.jsx
+++ src/components/userOptions/userOptions.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const UserOptions = (props) => {
-    const { id, firstName, lastName, email, role, active } = props;
+    const { id, firstName, lastName, email, role, active,fetchData } = props;
     const navigate = useNavigate();
     
     const handleConfirm = () => {
         navigate(`/admin-dashboard/users/${id}/reset-password`);
     };
+
+    const deactivateUser = async (id) => {
+        try {
+            const token = sessionStorage.getItem("token");
+            await axios.post(`http://localhost:8080/api/v1/admin/change-activation-user/`+id, {}, {
+                headers: {
+                    Authorization: `Bearer `+token,
+                },
+            });
+            await fetchData();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    const handleDeactivate = () => {
+        console.log(id);
+        deactivateUser(id);
+    }
+
+
 
 
     return (
@@ -23,13 +46,14 @@ const UserOptions = (props) => {
                     flexDirection: 'column',
                 }}>
                    
+                    <Typography variant='h6'>ID: {id}</Typography>
                     <Typography variant='h6'>First Name: {firstName}</Typography>
                     <Typography variant='h6'>Last Name: {lastName}</Typography>
                     <Typography variant='h6'>Email: {email}</Typography>
                     <Typography variant='h6'>Role: {role}</Typography>
                     <Typography variant='h6'>Active: {active ? 'Yes' : 'No'}</Typography>
                     <Button onClick={handleConfirm} variant='contained' style={{ margin: '8px' }}>Reset Password</Button> {/* Add margin to the buttons */}
-                    <Button variant='contained' style={{ margin: '8px', backgroundColor: active ? '#c62828' : 'green' }}>{active ? 'Deactivate' : 'Activate'}</Button> {/* Add margin to the buttons */}
+                    <Button onClick={handleDeactivate} variant='contained' style={{ margin: '8px', backgroundColor: active ? '#c62828' : 'green' }}>{active ? 'Deactivate' : 'Activate'}</Button> {/* Add margin to the buttons */}
                 </div>
             );
         };
