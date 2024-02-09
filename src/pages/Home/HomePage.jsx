655e8ca51485589620eb6c3e335c9109e49e1e54
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-02-09 13:08:51 -0600

    Homepage fixes

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,24 @@
 import React from "react"
-import { Box } from "@mui/material"
+import { useNavigate } from "react-router-dom"
+import { Box, Button, Typography } from "@mui/material"
+import { styled } from "@mui/material/styles"
+
 import SearchBar from "../../components/searchBar/SearchBar"
 import banner from "../../assets/banner.jpg"
 import Features from "../../components/features/Features"
 
+const StyledButton = styled(Button)({
+  color: "white",
+  height: 50,
+  textTransform: "uppercase",
+  "&:hover": {
+    color: "grey"
+  }
+})
+
 const HomePage = () => {
+  const navigate = useNavigate()
+
   return (
     <>
       <Box
@@ -22,7 +36,29 @@ const HomePage = () => {
           justifyContent: "center"
         }}
       >
-        <SearchBar />
+        <Box>
+          <Typography
+            variant="h3"
+            component="div"
+            sx={{
+              fontWeight: "bold",
+              mb: 2,
+              color: "white",
+              textShadow: "2px 2px 8px #000"
+            }}
+          >
+            Find your the right property of your dreams with Cofofo,
+          </Typography>
+          <Typography variant="h5" sx={{ mb: 3, color: "white", textShadow: "2px 2px 8px #000" }}>
+            Let's start exploring . . .
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <StyledButton variant="contained" size="large" onClick={() => navigate("/properties")}>
+              View Properties
+            </StyledButton>
+          </Box>
+          {/* <SearchBar /> */}
+        </Box>
       </Box>
       <Box>
         <Features />
--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -4,89 +4,44 @@ import PropertyCard from "../../components/cards/PropertyCard"
 import { dummyPropertiesData } from "../../dummy/PropertiesDummy"
 import FilterBy from "../../components/filterBy/FilterBy"
 import axios from "axios"
-import { useParams } from "react-router-dom"
 
 const Properties = () => {
+  const [propertyDetail, setPropertyDetail] = useState([])
 
-  
-  const [propertyDetail, setPropertyDetail] = useState([]);  
-  
-
-  const token = sessionStorage.getItem("token");
-  console.log("<<token    >>"+token)
-  
   useEffect(() => {
+    const token = sessionStorage.getItem("token")
     const fetchPropertiesData = async () => {
-        try {
-          
-            const response = await axios.get("http://localhost:8080/api/v1/properties", {
-
-              headers: {
-                Authorization: `Bearer ${token}`,
-            },
-          });
-          setPropertyDetail(response.data);
-
-        } catch (error) {
-          console.error(error);
+      try {
+        const response = await axios.get("http://localhost:8080/api/v1/properties", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        setPropertyDetail(response.data)
+      } catch (error) {
+        console.error(error)
       }
-    }; 
-    fetchPropertiesData();
-}, []);
-console.log("Properties <<>>>"+ propertyDetail)
+    }
+    fetchPropertiesData()
+  }, [])
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//         try {
-//             const token = sessionStorage.getItem("token");
-
-//             console.log("param>>"+param.dealType+"token>>>"+token);
-//             const response = await axios.get("http://localhost:8080/api/v1/properties/customers?dealtype="+param.dealType, {
-//                 headers: {
-//                     Authorization: `Bearer ${token}`,
-//                 },
-//             });
-//             setProps(response.data);
-//             console.log("Result" + response.data);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
-
-//     fetchData();
-// }, [param.dealType]);
-
-const handleSearch = (filteredData) => {
-  setPropertyDetail(filteredData);
-};
+  const handleSearch = filteredData => {
+    setPropertyDetail(filteredData)
+  }
 
   return (
     <>
-      <FilterBy onSearch={handleSearch}/>
-      <FilterBy onSearch={handleSearch}/>
+      <FilterBy onSearch={handleSearch} />
       <Container>
         <div>Properties</div>
         <Grid container spacing={4}>
-          {propertyDetail.map(p => (            
+          {propertyDetail.map(p => (
             <PropertyCard id={p.id} key={p.id} images={p.images} address={p.address} price={p.price} numberOfBed={p.numberOfBed} numberOfBathroom={p.numberOfBathroom} homeType={p.homeType} dealType={p.dealType} area={p.area} />
-          // {props.map(p => (
-          //   <PropertyCard
-          //     key={p.id}
-          //     id={p.id}
-          //     images={p.image}
-          //     address={p.address}
-          //     price={p.price}
-          //     numberOfBed={p.numberOfBed}
-          //     numberOfBathroom={p.numberOfBathroom}
-          //     homeType={p.homeType}
-          //     dealType={p.dealType}
-          //     area={p.area}
-          //   />
           ))}
         </Grid>
       </Container>
     </>
-  );
+  )
 }
 
-export default Properties;
+export default Properties
